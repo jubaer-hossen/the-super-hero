152472ed22cd8825f0b57ee9fd77538150670571
jubaer-hossen <jobaer1618@gmail.com>
2021-09-26 03:41:38 +0600

    added company name component

--- src/component/cart/Cart.js
+++ src/component/cart/Cart.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
 const Cart = props => {
-    console.log(props.name);
-    // console.log(props.company);
     const { cart } = props;
+    // console.log(cart);
     let total = 0;
+    let company1 = [];
     for (const company of cart) {
         total = total + company.cost;
+        company1 += company.company + ',' + ' ';
     }
+    // console.log(cart);
     return (
-        <div className="bg-primary ps-3 pe-5 py-5 ms-3 text-white rounded position-fixed">
+        <div className="bg-primary ps-3 pe-5 py-5 ms-3 text-white rounded">
             <h3>Total Company: {cart.length}</h3>
             <h3>Total Cost: ${total}</h3>
-            <h3 className="text-center"> Companies name: {props.company}</h3>
+            <br />
+            <h3 className="text-center"> Companies name: {company1}</h3>
         </div>
     );
 };
