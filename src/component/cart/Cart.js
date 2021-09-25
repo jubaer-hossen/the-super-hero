9b97274bb499b33a031b6b355e0b048b85b89d97
jubaer-hossen <jobaer1618@gmail.com>
2021-09-26 02:58:54 +0600

    edited readme file

--- src/component/cart/Cart.js
+++ src/component/cart/Cart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Cart = props => {
+    console.log(props.name);
     // console.log(props.company);
     const { cart } = props;
     let total = 0;
@@ -8,7 +9,7 @@ const Cart = props => {
         total = total + company.cost;
     }
     return (
-        <div>
+        <div className="bg-primary p-3 text-white rounded">
             <h3>Total Company: {cart.length}</h3>
             <h3>Total Cost: ${total}</h3>
             <h3> company: {props.company}</h3>
--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="bg-secondary text-white p-3 mb-2 text-center">
-            <h1>This is a powerful business plan with many companies</h1>
+            <h1>Make a powerful business team</h1>
+            <h4>This is a powerful business plan with many companies</h4>
             <h2>
                 Total Budget: <span className="fw-bold">$100M</span>
             </h2>
