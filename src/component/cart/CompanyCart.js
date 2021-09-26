4b5e7e45840737fe2d34c5fa84b65b0f9f0ca2b7
jubaer-hossen <jobaer1618@gmail.com>
2021-09-26 22:07:45 +0600

    edited style

--- src/component/cart/CompanyCart.js
+++ src/component/cart/CompanyCart.js
@@ -4,11 +4,11 @@ const CompanyCart = props => {
     // console.log(props.singleCart);
     const { company, pic } = props.singleCart;
     return (
-        <div className="d-flex mt-3 py-3 ps-3 pe-5 bg-info rounded text-white align-items-center">
+        <div className="d-flex mt-3 py-3 ps-3 pe-3 bg-info rounded text-white align-items-center">
             <img className="me-3 rounded" src={pic} alt="" />
             <h4>
                 <small>
-                    {company}
+                    {company}{' '}
                     <i className="fas fa-check-circle text-warning"></i>
                 </small>
             </h4>
